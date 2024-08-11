6cafe988aa9f8df35d669ec76ab0020d32e5fdee
Adrien <amaugueret@gmail.com>
2024-08-12 01:23:18 +0200

    added dots

--- src/navigation.js
+++ src/navigation.js
@@ -4,6 +4,7 @@ import { getTotalPictures } from "./picture";
 export default function startSlideshow() {
   let slideshow = new Slideshow();
   changeSlides(slideshow);
+  manageDots(slideshow, getTotalPictures());
 }
 
 function changeSlides(Slideshow) {
@@ -48,4 +49,44 @@ class Slideshow {
       this.currIndex -= getTotalPictures();
     }
   }
+}
+
+function createDot(index) {
+  const dot = document.createElement("div");
+  dot.classList.add("dot");
+  dot.setAttribute("id", index);
+  return dot;
+}
+
+function initDots(dotDiv, num) {
+  const fragment = document.createDocumentFragment();
+  let dotsArray = [];
+
+  for (let i = 0; i < num; i++) {
+    const dot = createDot(i);
+    fragment.appendChild(dot);
+    dotsArray.push(dot);
+  }
+
+  dotDiv.appendChild(fragment);
+  dotsArray[0].classList.add("selected");
+  return dotsArray;
+}
+
+function manageDots(slideshow, num) {
+  const dotDiv = document.querySelector(".dots");
+  const dots = initDots(dotDiv, num);
+
+  dotDiv.addEventListener("click", (event) => {
+    const targetDot = event.target;
+    if (targetDot.classList.contains("dot")) {
+      const selectedDot = document.querySelector(".selected");
+      if (selectedDot) {
+        selectedDot.classList.remove("selected");
+      }
+      targetDot.classList.add("selected");
+      const index = Array.from(dots).indexOf(targetDot);
+      slideshow.updateCarousel(index);
+    }
+  });
 }
